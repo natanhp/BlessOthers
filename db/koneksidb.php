9787202fe62e329663e3175edb61916c7b1a5223
Natan Hari Pamungkas <natanhp@protonmail.com>
2019-04-06 02:16:21 +0700

    Mengganti kode dengan contoh di azure

--- db/koneksidb.php
+++ db/koneksidb.php
@@ -4,10 +4,19 @@
 	$password = "***REMOVED***";
 	$db = "dicodingwebapp";
 
-	try {
-		$conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
-		$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-	} catch(Exception $e) {
-			echo "Failed: " . $e;
+	// PHP Data Objects(PDO) Sample Code:
+try {
+	$conn = new PDO("sqlsrv:server = tcp:dicodingdatabase.database.windows.net,1433; Database = dicodingwebapp", "natanhp", $password);
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	}
+	catch (PDOException $e) {
+		print("Error connecting to SQL Server.");
+		die(print_r($e));
+	}
+
+	// SQL Server Extension Sample Code:
+	$connectionInfo = array("UID" => "natanhp@dicodingdatabase", "pwd" => $password, "Database" => "dicodingwebapp", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+	$serverName = "tcp:dicodingdatabase.database.windows.net,1433";
+	$conn = sqlsrv_connect($serverName, $connectionInfo);
+
 ?>
