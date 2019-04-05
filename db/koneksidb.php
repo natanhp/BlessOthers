c8154bad7e7fc076acf9ffad7988fae08a2c20d1
Natan Hari Pamungkas <natanhp@protonmail.com>
2019-04-06 00:54:39 +0700

    Membuat koneksi database

---
+++ db/koneksidb.php
@@ -0,0 +1,15 @@
+<?php
+	$host = "dicodingdatabase.database.windows.net";
+	$user = "natanhp";
+	$password = "***REMOVED***";
+	$db = "dicodingwebapp";
+
+	try {
+		$conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
+		$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+} catch(Exception $e) {
+		echo "Failed: " . $e;
+}default{
+	echo "Iso bosq";
+}
+?>
