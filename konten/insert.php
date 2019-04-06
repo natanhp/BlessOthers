a6fc728d98efc790a6be8042b4265898fd9d3fef
Natan Hari Pamungkas <natanhp@protonmail.com>
2019-04-06 16:26:26 +0000

    Check koneksi

--- konten/insert.php
+++ konten/insert.php
@@ -1,29 +1,33 @@
 <?php
 	include ('../db/koneksidb.php');
 
-	echo isset($_POST['submit']);
+	echo $conn;
 
-	if(isset($_POST['submit'])){
-		try{
-			// $nama=$_POST['nama'];
-			// $words=$_POST['words'];
-
-			// echo $nama;
-			// echo $words;
-			
-			// $q = "INSERT INTO [dbo].[bless] (nama, words) VALUES (:nama, :words)";
-			// $query = $db->prepare($q);
-			// $result = $query->execute(array(":nama" => $nama, ":words" => $words));
-
-			$sql_select = "SELECT * FROM [dbo].[bless]";
+	$sql_select = "SELECT * FROM [dbo].[bless]";
             $stmt = $conn->query($sql_select);
 			$registrants = $stmt->fetchAll(); 
+
+			echo $stmt . $registrants;
 			
 			foreach($registrants as $test){
 				echo $test['nama'] . $test['words'];
 			}
-		}catch(Exception $e){
-			echo "Failed: " . $e;
-		}
-	}
+
+	// if(isset($_POST['submit'])){
+	// 	try{
+	// 		// $nama=$_POST['nama'];
+	// 		// $words=$_POST['words'];
+
+	// 		// echo $nama;
+	// 		// echo $words;
+			
+	// 		// $q = "INSERT INTO [dbo].[bless] (nama, words) VALUES (:nama, :words)";
+	// 		// $query = $db->prepare($q);
+	// 		// $result = $query->execute(array(":nama" => $nama, ":words" => $words));
+
+			
+	// 	}catch(Exception $e){
+	// 		echo "Failed: " . $e;
+	// 	}
+	// }
 ?>
