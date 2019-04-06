1c28b34d8690f82d590d3bdf1f1dfb9d3a359e99
Natan Hari Pamungkas <natanhp@protonmail.com>
2019-04-06 15:28:47 +0000

    Membuat fungsi untuk insert data

---
+++ konten/insert.php
@@ -0,0 +1,18 @@
+<?php
+	include('db/koneksidb.php');
+
+	if(isset($_POST['submit'])){
+		try{
+			$nama=$_POST['nama'];
+			$words=$_POST['words'];
+
+			$sql_insert = "INSERT INTO bless (nama, words) VALUES (?,?)";
+			$statement = $conn->prepare($sql_insert);
+			$statement->bindValue(1, $nama);
+			$statement->bindValue(2, $words);
+			$statement->execute();
+		}catch(Exception $e){
+			echo "Failed: " . $e;
+		}
+	}
+?>
