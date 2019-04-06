8528e1cbfd6af3d20d9ce9bffffbb0d8af67bcea
Natan Hari Pamungkas <natanhp@protonmail.com>
2019-04-06 15:51:08 +0000

    Test koneksi

--- konten/insert.php
+++ konten/insert.php
@@ -1,22 +1,8 @@
 <?php
-	// include 'db/koneksidb.php';
-	$password = "***REMOVED***";
+	include 'db/koneksidb.php';
 
-	// PHP Data Objects(PDO) Sample Code:
-try {
-	$conn = new PDO("sqlsrv:server = tcp:dicodingdatabase.database.windows.net,1433; Database = dicodingwebapp", "natanhp", $password);
-	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	}
-	catch (PDOException $e) {
-		print("Error connecting to SQL Server.");
-		die(print_r($e));
-	}
-
-	// SQL Server Extension Sample Code:
-	$connectionInfo = array("UID" => "natanhp@dicodingdatabase", "pwd" => $password, "Database" => "dicodingwebapp", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-	$serverName = "tcp:dicodingdatabase.database.windows.net,1433";
-	$conn = sqlsrv_connect($serverName, $connectionInfo);
-
+	echo $conn;
+	
 	if(isset($_POST['submit'])){
 		try{
 			$nama=$_POST['nama'];
