270ef819500c0d895cffbc01310885c56b22974d
Natan Hari Pamungkas <natanhp@protonmail.com>
2019-04-06 16:00:32 +0000

    Memperbaiki statment

--- konten/insert.php
+++ konten/insert.php
@@ -2,17 +2,16 @@
 	include ('../db/koneksidb.php');
 
 	echo isset($_POST['submit']);
-	
+
 	if(isset($_POST['submit'])){
 		try{
 			$nama=$_POST['nama'];
 			$words=$_POST['words'];
 
-			$sql_insert = "INSERT INTO bless (nama, words) VALUES (?,?)";
-			$statement = $conn->prepare($sql_insert);
-			$statement->bindValue(1, $nama);
-			$statement->bindValue(2, $words);
-			$statement->execute();
+			$stmt = $conn->prepare($sql_insert);
+            $stmt->bindValue(1, $nama);
+            $stmt->bindValue(2, $words);
+            $stmt->execute();
 		}catch(Exception $e){
 			echo "Failed: " . $e;
 		}
