a16365342bbb42582078a69d60b72d9573bfd855
Natan Hari Pamungkas <natanhp@protonmail.com>
2019-04-06 16:23:21 +0000

    Menguji query

--- konten/insert.php
+++ konten/insert.php
@@ -5,15 +5,23 @@
 
 	if(isset($_POST['submit'])){
 		try{
-			$nama=$_POST['nama'];
-			$words=$_POST['words'];
+			// $nama=$_POST['nama'];
+			// $words=$_POST['words'];
 
-			echo $nama;
-			echo $words;
+			// echo $nama;
+			// echo $words;
 			
-			$q = "INSERT INTO [dbo].[bless] (nama, words) VALUES (:nama, :words)";
-			$query = $db->prepare($q);
-			$result = $query->execute(array(":nama" => $nama, ":words" => $words));
+			// $q = "INSERT INTO [dbo].[bless] (nama, words) VALUES (:nama, :words)";
+			// $query = $db->prepare($q);
+			// $result = $query->execute(array(":nama" => $nama, ":words" => $words));
+
+			$sql_select = "SELECT * FROM [dbo].[bless]";
+            $stmt = $conn->query($sql_select);
+			$registrants = $stmt->fetchAll(); 
+			
+			foreach($registrants as $test){
+				echo $test['nama'] . $test['words'];
+			}
 		}catch(Exception $e){
 			echo "Failed: " . $e;
 		}
