05ee230d0a5ef60595d3e30b3581ae710d40df68
Natan Hari Pamungkas <natanhp@protonmail.com>
2019-04-08 14:04:43 +0700

    Menghapus kode yang tidak perlu

--- db/koneksidb.php
+++ db/koneksidb.php
@@ -10,9 +10,4 @@ try {
 		print("Error connecting to SQL Server.");
 		die(print_r($e));
 	}
-
-	// // SQL Server Extension Sample Code:
-	// $connectionInfo = array("UID" => "natanhp@dicodingdatabase", "pwd" => $password, "Database" => "dicodingwebapp", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-	// $serverName = "tcp:dicodingdatabase.database.windows.net,1433";
-	// $conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
