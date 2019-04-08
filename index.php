201bac143c5d8c5b2fc444d9d36f685f8eb8b3a7
Natan Hari Pamungkas <natanhp@protonmail.com>
2019-04-08 13:45:22 +0700

    Membenarkan query

--- index.php
+++ index.php
@@ -50,7 +50,7 @@
 			echo $nama;
 			echo $words;
 			
-			$query = "INSERT INTO [dbo].[bless] (nama, words) VALUES (:nama, :words)";
+			$query = "INSERT INTO bless (nama, words) VALUES (:nama, :words)";
 			$query = $conn->prepare($q);
 			$result = $query->execute(array(":nama" => $nama, ":words" => $words));
 
