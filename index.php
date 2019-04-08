9c2a9c966c928f0016a1a2c88bd2999629d37289
Natan Hari Pamungkas <natanhp@protonmail.com>
2019-04-08 13:48:15 +0700

    Mengganti kode inserrt

--- index.php
+++ index.php
@@ -50,11 +50,16 @@
 			echo $nama;
 			echo $words;
 			
-			$query = "INSERT INTO bless (nama, words) VALUES (:nama, :words)";
-			$query = $conn->prepare($q);
-			$result = $query->execute(array(":nama" => $nama, ":words" => $words));
+			// $query = "INSERT INTO bless (nama, words) VALUES (:nama, :words)";
+			// $query = $conn->prepare($q);
+			// $result = $query->execute(array(":nama" => $nama, ":words" => $words));
 
 			
+			$sql_insert = "INSERT INTO bless (nama, words) VALUES (?,?)";
+			$stmt = $conn->prepare($sql_insert);
+			$stmt->bindValue(1, $nama);
+			$stmt->bindValue(2, $words);
+			$stmt->execute();
 		}catch(Exception $e){
 			echo "Failed: " . $e;
 		}
