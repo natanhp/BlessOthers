13e531b89ad89596ddedfc4210db6bde6d17f21e
Natan Hari Pamungkas <natanhp@protonmail.com>
2019-04-08 13:35:23 +0700

    Mengedit php untuk insert

--- index.php
+++ index.php
@@ -41,22 +41,37 @@
 
 <?php
 	include('db/koneksidb.php');
-	
-	if(isset($_POST['submit'])){
-		try{
-			$nama=$_POST['nama'];
-			$words=$_POST['words'];
 
-			echo $nama;
-			echo $words;
+	// if(isset($_POST['submit'])){
+	// 	try{
+	// 		$nama=$_POST['nama'];
+	// 		$words=$_POST['words'];
+
+	// 		echo $nama;
+	// 		echo $words;
 			
-			$q = "INSERT INTO [dbo].[bless] (nama, words) VALUES (:nama, :words)";
-			$query = $db->prepare($q);
-			$result = $query->execute(array(":nama" => $nama, ":words" => $words));
+	// 		$q = "INSERT INTO [dbo].[bless] (nama, words) VALUES (:nama, :words)";
+	// 		$query = $db->prepare($q);
+	// 		$result = $query->execute(array(":nama" => $nama, ":words" => $words));
 
 			
-		}catch(Exception $e){
-			echo "Failed: " . $e;
+	// 	}catch(Exception $e){
+	// 		echo "Failed: " . $e;
+	// 	}
+	// }
+	if (isset($_POST['submit'])) {
+		try {
+				$name = $_POST['nama'];
+				$words = $_POST['words'];
+				// Insert data
+				$sql_insert = "INSERT INTO bless (nama, words) 
+										VALUES (?,?)";
+				$stmt = $conn->prepare($sql_insert);
+				$stmt->bindValue(1, $nama);
+				$stmt->bindValue(2, $words);
+				$stmt->execute();
+		} catch(Exception $e) {
+				echo "Failed: " . $e;
 		}
 	}
 ?>
