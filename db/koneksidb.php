2bd7b94d7aa53b2d7b04b6de7d025e7a4c918db5
Natan Hari Pamungkas <natanhp@protonmail.com>
2019-04-08 14:08:06 +0700

    Merapikan kode

--- db/koneksidb.php
+++ db/koneksidb.php
@@ -5,9 +5,9 @@
 try {
 	$conn = new PDO("sqlsrv:server = tcp:dicodingdatabase.database.windows.net,1433; Database = dicodingwebapp", "natanhp", $password);
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	}
-	catch (PDOException $e) {
-		print("Error connecting to SQL Server.");
-		die(print_r($e));
-	}
+}
+catch (PDOException $e) {
+	print("Error connecting to SQL Server.");
+	die(print_r($e));
+}
 ?>
